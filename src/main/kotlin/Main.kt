a9ee33cbe6867edca8f78d0b310225c7639f219c
Polina Vnukova <vnukovapdeveloper@gmail.com>
2024-08-16 11:25:15 +0300

    11 exception of rule added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 package ru.netology
 
 fun main() {
-    val likes = 21
+    val likes = 11
 
-    if (likes % 10 == 1) {
+    if (likes % 10 == 1 && likes % 100 != 11) {
         println("Понравилость $likes человеку")
     }
     else {
